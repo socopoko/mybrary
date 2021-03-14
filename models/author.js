9dd6c9e22af02939481ca371e9025449d8e9838a
socopoko <59167401+socopoko@users.noreply.github.com>
2021-03-14 17:54:50 +0530

    Books/Create/Index/New

--- models/author.js
+++ models/author.js
@@ -1,10 +1,10 @@
 const mongoose = require('mongoose')
 
-const authoSchema = new mongoose.Schema({
+const authorSchema = new mongoose.Schema({
     name: {
         type: String,
         required: true,
     }
 })
 
-module.exports = mongoose.model('Author', authoSchema)
+module.exports = mongoose.model('Author', authorSchema)
